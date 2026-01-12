f552699f988e44d392825c291214d2c69f96ec91
ritikmishra7488@gmail.com <ritikmishra7488@gmail.com>
2026-01-12 21:19:35 +0530

    For the given CP and SP. Calculate %Profit. CP=120; SP=160;

---
+++ Arithmetic Operator/Q6.java
@@ -0,0 +1,11 @@
+public class Q6 {
+    public static void main(String[] args) {
+        int cp=120;
+        int sp=160;
+        int profit=sp-cp;
+        int profitPercentage=(profit*100)/cp;
+        System.out.println(profit);
+        System.out.println(profitPercentage);
+    }
+    
+}
