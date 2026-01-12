a520718bdf52caa3c4179474cca7f6cb25054b99
ritikmishra7488@gmail.com <ritikmishra7488@gmail.com>
2026-01-12 21:15:45 +0530

    For the given CP and SP. Calculate %Loss. CP=120; SP=90;

---
+++ Arithmetic Operator/Q7.java
@@ -0,0 +1,9 @@
+public class Q7 {
+    public static void main(String[] args) {
+        int cp=120;
+        int sp=90;
+        int loss=cp-sp;
+        int lossPercentage=(loss*100)/cp;
+        System.out.println(lossPercentage);
+    }    
+}
